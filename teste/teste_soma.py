fd2b93b18ec56328a7fb26a73ce13aacbabef42a
rossi1501 <douglas.alexandre.rossi@gmail.com>
2020-12-02 13:56:41 -0300

    0

--- teste/teste_soma.py
+++ teste/teste_soma.py
@@ -1,7 +1,7 @@
-import unittest
+import pytest
 from src.main import soma
 
 
-class TestSoma(unittest.TesteCase):
+class TestSoma(pytest.TesteCase):
     def test_retorno_soma_10_10(self):
         self.assertEqual(soma(10, 10), 20)
